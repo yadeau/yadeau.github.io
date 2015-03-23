c33f46b15c30045709bdc8711db968fbe070814a
yadeau <yadea003@morris.umn.edu>
2015-03-22 20:52:28 -0500

    Moving on, will try again later

--- scripts.js
+++
@@ -1,9 +0,0 @@
-var app = angular.module('angularjs-starter', []);
-
-app.controller('MainCtrl', function($scope, $location, $anchorScroll) {
-  $scope.scrollTo = function(id) {
-    $location.hash(id);
-    console.log($location.hash());
-    $anchorScroll();
-  };
-});
